bf26183135af3165a1ad8f409c80065f31a13379
Naseem Oyebola <naseemoyebola@gmail.com>
2023-07-03 14:50:59 +0100

    initial commit

---
+++ RxPrac/RxPrac/MyTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxPrac
+//
+//  Created by Naseem Oyebola on 03/07/2023.
+//
+
+import UIKit
+
+class MyTableViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    view.backgroundColor = .blue
+  }
+
+
+}
+
